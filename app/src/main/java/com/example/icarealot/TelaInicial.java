129abf2c842f46832e66310efd0c1c2f06234230
LeonardoEckertMarques <leonardoeckert.contato@gmail.com>
2021-05-12 20:13:06 -0300

    Alexsander

--- app/src/main/java/com/example/icarealot/TelaInicial.java
+++ app/src/main/java/com/example/icarealot/TelaInicial.java
@@ -11,4 +11,7 @@ public class TelaInicial extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_tela_inicial);
   }
+
+  // Teste Alexsander
+
 }
